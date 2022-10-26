caebde0cd998b40c860575338db4a8d9f8c74ce2
MoiseenkoEvgenia <sokolova-2206@mail.ru>
2022-10-26 23:22:39 +0300

    add Zadacha 19 палиндром

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("Введите слово");
+string text = Console.ReadLine();
+int i = 0, j = text.Length - 1;
+while ((i<j) && (text[i] == text[j]))
+{
+    i++; j--;
+}
+if(text[i] == text[j])
+Console.WriteLine(text +" - это палиндром!");
+else
+Console.WriteLine(text +" - это не палиндром!");
